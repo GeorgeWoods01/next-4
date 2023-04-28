69c450f790b32bdfdd766d995ec48d95244e4100
george woods <georgewoods01@gmail.com>
2023-04-28 15:36:02 +0200

    added delete in crud operations, still some issues with a 404

--- src/components/recipes/RecipeItem.js
+++ src/components/recipes/RecipeItem.js
@@ -1,8 +1,19 @@
 import classes from "./RecipeItem.module.css";
 import Card from "../ui/Card";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 function RecipeItem(props) {
+  const router = useRouter();
+  const deleteHandler = async () => {
+    const response = await fetch(`/api/delete-recipe/${props.id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      router.push("/recipes");
+    }
+  };
+
   return (
     <li className={classes.item}>
       <Card>
@@ -15,6 +26,7 @@ function RecipeItem(props) {
         </div>
         <div className={classes.actions}>
           <button>Show Details</button>
+          <button onClick={() => deleteHandler(id)}>Delete recipe</button>
         </div>
       </Card>
     </li>
---
+++ src/pages/api/delete-recipe.js
@@ -0,0 +1,23 @@
+import { MongoClient, ObjectId } from "mongodb";
+
+async function handler(req, res) {
+  const recipeId = req.query.recipeId;
+
+  if (req.method === "DELETE") {
+    const client = await MongoClient.connect(process.env.API_KEY);
+    const db = client.db();
+    const recipesCollection = db.collection("recipes");
+
+    const result = await recipesCollection.deleteOne({
+      _id: new ObjectId(recipeId),
+    });
+    console.log(result);
+    console.log("success");
+
+    client.close();
+
+    res.status(200).json({ message: "Recipe deleted successfully." });
+  }
+}
+
+export default handler;
