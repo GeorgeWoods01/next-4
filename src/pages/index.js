4c940a2a6e8b3c9b3339d8418759ad4342d7c1be
george woods <georgewoods01@gmail.com>
2023-05-04 11:28:29 +0200

    simple recipe search function added to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,27 @@
 import RecipeList from "../components/recipes/RecipeList";
 import { MongoClient } from "mongodb";
 import Head from "next/head";
-
-
+import { useState, useEffect } from "react";
 
 export default function Home(props) {
- return (
-   <RecipeList recipes={props.recipes} />
+  const [query, setQuery] = useState("");
+  const [fileteredRecipes, setFilteredRecipes] = useState(props.recipes);
+  useEffect(() => {
+    const filtered = props.recipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredRecipes(filtered);
+  }, [query, props.recipes]);
+  return (
+    <>
+      <input type="text" placeholder="Search recipes" onChange={(e) => setQuery(e.target.value)} />
+      <RecipeList recipes={fileteredRecipes} />
+    </>
   );
 }
 
 export async function getStaticProps() {
-  const client = await MongoClient.connect(
-    process.env.API_KEY
-  );
+  const client = await MongoClient.connect(process.env.API_KEY);
   const db = client.db();
   const recipesCollection = db.collection("recipes"); //connect to db
 
@@ -34,9 +42,6 @@ export async function getStaticProps() {
     revalidate: 10,
   };
 }
-
-
-
 
 // const DUMMY_DATA = [
 //   {
