0317329ee3eeca8fb5ad78934f1e8b74ac9bc2f5
george woods <georgewoods01@gmail.com>
2023-05-05 17:36:23 +0200

    adding recipe editing functionality, most code there, still needs tweaking

--- src/components/recipes/RecipeItem.js
+++ src/components/recipes/RecipeItem.js
@@ -16,6 +16,10 @@ function RecipeItem(props) {
   function showDetailsHandler() {
     router.push('/' + props.id)
   }
+  function editRecipeHandler() {
+    router.push(`/edit-recipe/${props.id}`);
+  }
+
 
   return (
     <li className={classes.item}>
@@ -33,6 +37,7 @@ function RecipeItem(props) {
         <div className={classes.actions}>
           <button onClick={showDetailsHandler}>Show Details</button>
           <button onClick={deleteHandler}>Delete recipe</button>
+          <button onClick={editRecipeHandler}>Edit Recipe</button>
         </div>
       </Card>
     </li>
